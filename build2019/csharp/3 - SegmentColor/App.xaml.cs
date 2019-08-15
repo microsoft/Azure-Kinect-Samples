6329bb83d43b28181d6539c9aa313181150ab042
Chris Edmonds <cedmonds@microsoft.com>
2019-08-14 17:51:49 -0700

    Initial commit of C# //BUILD samples

---
+++ build2019/csharp/3 - SegmentColor/App.xaml.cs
@@ -0,0 +1,18 @@
+﻿//------------------------------------------------------------------------------
+// <copyright file="App.xaml.cs" company="Microsoft">
+//     Copyright (c) Microsoft Corporation.  All rights reserved.
+// </copyright>
+//------------------------------------------------------------------------------
+
+namespace Microsoft.Samples.Kinect.CoordinateMappingBasics
+{
+    using System;
+    using System.Windows;
+
+    /// <summary>
+    /// Interaction logic for App
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
